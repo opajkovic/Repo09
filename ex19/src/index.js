a885542d191ea745bd01bff409744ae98a30c228
opajkovic <oljaolja170@gmail.com>
2022-04-05 22:42:32 +0200

    added ex19

---
+++ ex19/src/index.js
@@ -0,0 +1,14 @@
+function myDoWhile() {
+    var myNumbers = "";
+    var i = 0;
+    do {
+        myNumbers = myNumbers + i +",";
+        i++;
+    } while ( i < 9) {
+       myNumbers = myNumbers  + i;
+    }
+    return myNumbers;
+}
+console.log(myDoWhile());
+myDoWhile();
+module.exports = myDoWhile;
