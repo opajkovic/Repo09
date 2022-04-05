57622aba19870d35a49423fe8424c2a7c11ba2dd
opajkovic <oljaolja170@gmail.com>
2022-04-05 10:33:25 +0200

    added ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,17 @@
+function compareStrictValues(m) {
+    if(m != 55) {
+        return "Not equal";
+    }
+    return "Equal";
+}
+console.log(compareStrictValues(55));
+console.log(compareStrictValues("55"));
+console.log(compareStrictValues("21"));
+console.log(compareStrictValues(12));
+console.log(compareStrictValues("Doe"));
+compareStrictValues(55);
+compareStrictValues("55");
+compareStrictValues("21");
+compareStrictValues(12);
+compareStrictValues("Doe");
+module.exports = compareStrictValues;
