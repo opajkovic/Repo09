9e81d1edb002a75302c69e3085970b03119002b5
opajkovic <oljaolja170@gmail.com>
2022-04-05 21:54:31 +0200

    added

--- ex17/src/index.js
+++ ex17/src/index.js
@@ -1,11 +1,14 @@
 function inverseWhile() {
     var fiveNumbers = "";
     var i = 5;
-    while (i > 0) {
-        fiveNumbers = fiveNumbers + i + ",";
+    while (i >= 0) {
+        if(fiveNumbers == 0) {
+            fiveNumbers = fiveNumbers + i;
+        } else {
+            fiveNumbers = fiveNumbers + ","+ i;
+        }
         i--;
-    }
-    fiveNumbers = fiveNumbers + 0;
+     }
     return fiveNumbers;
 }
 console.log(inverseWhile());
